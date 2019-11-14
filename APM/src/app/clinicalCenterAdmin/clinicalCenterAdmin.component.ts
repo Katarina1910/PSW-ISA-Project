2855d03f1cb5518ba325885d2d3e51110b5493da
miilica <milicaradovanovic64@gmail.com>
2019-11-14 17:38:57 +0100

    front-end project

---
+++ APM/src/app/clinicalCenterAdmin/clinicalCenterAdmin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'cca-clincenadm',
+    templateUrl: './clinicalCenterAdmin.component.html'
+})
+
+export class ClinicalCenterAdministratorCompoment{
+
+}
