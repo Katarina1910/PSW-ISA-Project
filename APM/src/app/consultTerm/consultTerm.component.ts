6be685816d07124ac6e6fc0ab47d875d2e1d2e39
miilica <milicaradovanovic64@gmail.com>
2019-11-16 17:05:39 +0100

    request for conusult component

--- APM/src/app/consultTerm/consultTerm.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector : 'cc-consultTerm',
-    templateUrl : './consultTerm.component.html'
-})
-export class ConsultTermComponent{
-
-}
