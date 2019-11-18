948099520a0a017ca1162104847a788b0abfb427
miilica <milicaradovanovic64@gmail.com>
2019-11-18 16:39:14 +0100

    APM
    front-end

---
+++ APM/src/app/createDiagCodeBook/createDiagCodeBook.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'cca-crtdiagcdb',
+    templateUrl: './createDiagCodeBook.component.html'
+})
+
+export class createDiagnosisCodeBook{
+
+}
