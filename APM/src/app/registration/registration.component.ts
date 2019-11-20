75c5bacf93efcdfc3378c2bdec993a99a9bb401a
Katarina1910 <katarinaprodanovic97@gmail.com>
2019-11-20 13:24:12 +0100

    Front-end
    Some html pages added

--- APM/src/app/registration/registration.component.ts
+++ APM/src/app/registration/registration.component.ts
@@ -1,39 +1,16 @@
 import { Component } from '@angular/core';
 import { User } from './user';
 import { UserService } from './user.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
 
 @Component({
     selector : 'cc-registration',
     templateUrl : './registration.component.html'
 })
 export class RegistrationComponent{
-    userModel = new User("","","","","","","","","","","");
+    userModel = new User("hi"," "," "," "," "," "," "," "," "," "," ");
     
-    
-    registerForm: FormGroup;
-    submitted = false;
-    
-    constructor(private _userService: UserService, private formBuilder: FormBuilder) {}
+    constructor(private _userService: UserService) {}
 
-    ngOnInit() {
-        this.registerForm = this.formBuilder.group({
-            firstName: ['', Validators.required],
-            lastName: ['', Validators.required],
-            ucidn: ['', Validators.required],
-            address: ['', Validators.required],
-            city: ['', Validators.required],
-            country: ['', Validators.required],
-            phoneNumber: ['', Validators.required],
-            username: ['', Validators.required],
-            email: ['', [Validators.required, Validators.email]],
-            password: ['', [Validators.required, Validators.minLength(6)]]
-        });
-    }
-
-    // convenience getter for easy access to form fields
-    get f() { return this.registerForm.controls; }
     onSubmit(){
         this._userService.enroll(this.userModel)
         .subscribe(
@@ -41,17 +18,5 @@ export class RegistrationComponent{
             error=> console.error('Error!',error)
         )
     }
-
-        //validacija
-        this.submitted = true;
-
-        // stop here if form is invalid
-        if (this.registerForm.invalid) {
-            return;
-        }
-
-        alert('Registered')
-    }
-
 }
 
