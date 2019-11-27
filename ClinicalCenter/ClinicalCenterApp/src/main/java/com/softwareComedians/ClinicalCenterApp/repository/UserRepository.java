e0f075e79dc497d72a77cba1d4d58bc85f95e6d8
miilica <milicaradovanovic64@gmail.com>
2019-11-27 01:10:00 +0100

    add doctor service

--- ClinicalCenter/ClinicalCenterApp/src/main/java/com/softwareComedians/ClinicalCenterApp/repository/UserRepository.java
+++ ClinicalCenter/ClinicalCenterApp/src/main/java/com/softwareComedians/ClinicalCenterApp/repository/UserRepository.java
@@ -4,8 +4,8 @@ import com.softwareComedians.ClinicalCenterApp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 @Repository
+
 public interface UserRepository extends JpaRepository<User,Long> {
 
     User findByEmail(String email);
