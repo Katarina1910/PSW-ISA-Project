a27c9ab899cb16358b47a425ee5fbd63b7583dbe
miilica <milicaradovanovic64@gmail.com>
2019-12-02 21:15:10 +0100

    add RoomRepository class (back-end)

---
+++ ClinicalCenter/ClinicalCenterApp/src/main/java/com/softwareComedians/ClinicalCenterApp/repository/RoomRepository.java
@@ -0,0 +1,8 @@
+package com.softwareComedians.ClinicalCenterApp.repository;
+
+import com.softwareComedians.ClinicalCenterApp.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+}
+
