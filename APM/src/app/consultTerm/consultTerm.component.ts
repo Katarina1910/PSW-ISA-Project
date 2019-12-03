3877be695687f880804cbca979de47ddb4e1d47a
miilica <milicaradovanovic64@gmail.com>
2019-12-03 22:45:44 +0100

    add list of rooms to consult term component

--- APM/src/app/consultTerm/consultTerm.component.ts
+++ APM/src/app/consultTerm/consultTerm.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ConsultTerm } from './consultTerm';
 import { ConsultTermService } from './consultTerm.service';
 import { Router } from '@angular/router';
+import { Room } from '../rooms/room';
+import { DeleteRoomService } from '../rooms/deleteRoom.servce';
 
 @Component({
     selector : 'cc-consultTerm',
     templateUrl : './consultTerm.component.html'
 })
-export class ConsultTermComponent{
+export class ConsultTermComponent implements OnInit {
+    public rooms: Room[];
+    ngOnInit(): void {
+        this._deleteRoomService.getRooms().subscribe(
+            data=>{ 
+                this.rooms = data;
+            },
+            error=> console.error('Error!', error)
+        )
+    }
     consultTermModel = new ConsultTerm('','','','','');
     
-    constructor(private _consultTermService: ConsultTermService, private router:Router) {}
+    constructor(private _consultTermService: ConsultTermService, private router:Router,private _deleteRoomService: DeleteRoomService) {}
 
     onSubmit(){
         this._consultTermService.addConsultTerm(this.consultTermModel)
