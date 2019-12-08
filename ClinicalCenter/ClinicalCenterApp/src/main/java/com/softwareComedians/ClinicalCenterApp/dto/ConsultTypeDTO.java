c496748540a9c6a934075b1648a7176dbb1ff528
miilica <milicaradovanovic64@gmail.com>
2019-12-08 16:30:28 +0100

    add description to consult type model and dto class

--- ClinicalCenter/ClinicalCenterApp/src/main/java/com/softwareComedians/ClinicalCenterApp/dto/ConsultTypeDTO.java
+++ ClinicalCenter/ClinicalCenterApp/src/main/java/com/softwareComedians/ClinicalCenterApp/dto/ConsultTypeDTO.java
@@ -5,18 +5,29 @@ import com.softwareComedians.ClinicalCenterApp.model.ConsultType;
 public class ConsultTypeDTO {
     private  String name;
     private Long id;
+    private  String description;
 
     public ConsultTypeDTO() {
     }
 
-    public ConsultTypeDTO(String name, Long id) {
+    public ConsultTypeDTO(String name, Long id,String description) {
         this.name = name;
         this.id = id;
+        this.description = description;
     }
 
     public ConsultTypeDTO(ConsultType c){
         this.id = c.getId();
         this.name = c.getName();
+        this.description = c.getDescription();
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public String getName() {
