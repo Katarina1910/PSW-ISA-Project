86cd5f9f926a1317f6a59ecc5809faec027e04e9
miilica <milicaradovanovic64@gmail.com>
2019-12-08 16:28:17 +0100

    Create ConsultTypeService.java

---
+++ ClinicalCenter/ClinicalCenterApp/src/main/java/com/softwareComedians/ClinicalCenterApp/service/ConsultTypeService.java
@@ -0,0 +1,31 @@
+package com.softwareComedians.ClinicalCenterApp.service;
+
+import com.softwareComedians.ClinicalCenterApp.model.ConsultType;
+import com.softwareComedians.ClinicalCenterApp.repository.ConsultTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ConsultTypeService {
+    @Autowired
+    ConsultTypeRepository consultTypeRepository;
+
+    public ConsultType findOne(Long id){
+        return consultTypeRepository.findById(id).orElseGet(null);
+    }
+
+    public List<ConsultType> findAll(){
+        return consultTypeRepository.findAll();
+    }
+
+    public ConsultType save(ConsultType t) {
+        return consultTypeRepository.save(t);
+    }
+
+    public void remove(Long id){
+        consultTypeRepository.deleteById(id);
+    }
+
+}
