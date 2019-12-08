75e48036c036b920067b349959e4693b1a0419a3
miilica <milicaradovanovic64@gmail.com>
2019-12-08 16:28:11 +0100

    Create ConsultTypeRepository.java

---
+++ ClinicalCenter/ClinicalCenterApp/src/main/java/com/softwareComedians/ClinicalCenterApp/repository/ConsultTypeRepository.java
@@ -0,0 +1,9 @@
+package com.softwareComedians.ClinicalCenterApp.repository;
+
+import com.softwareComedians.ClinicalCenterApp.model.ConsultType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ConsultTypeRepository  extends JpaRepository<ConsultType,Long> {
+}
