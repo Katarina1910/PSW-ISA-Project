fdaee6c4d481cfd5f5cbc3da09c12c5bcfbdce20
miilica <milicaradovanovic64@gmail.com>
2019-12-09 00:15:20 +0100

    add finfByName method to consultType repository

--- ClinicalCenter/ClinicalCenterApp/src/main/java/com/softwareComedians/ClinicalCenterApp/repository/ConsultTypeRepository.java
+++ ClinicalCenter/ClinicalCenterApp/src/main/java/com/softwareComedians/ClinicalCenterApp/repository/ConsultTypeRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ConsultTypeRepository  extends JpaRepository<ConsultType,Long> {
+    ConsultType findByName(String name);
 }
