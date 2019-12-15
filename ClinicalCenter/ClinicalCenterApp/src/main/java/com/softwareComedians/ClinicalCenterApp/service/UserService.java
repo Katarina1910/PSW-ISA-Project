4c01b7d9060eb8cd2b64e1f69ba3d2b7eeca2ea8
miilica <milicaradovanovic64@gmail.com>
2019-12-15 18:01:38 +0100

    deleted unneccesary code

--- ClinicalCenter/ClinicalCenterApp/src/main/java/com/softwareComedians/ClinicalCenterApp/service/UserService.java
+++ ClinicalCenter/ClinicalCenterApp/src/main/java/com/softwareComedians/ClinicalCenterApp/service/UserService.java
@@ -23,57 +23,6 @@ public interface UserService {
     User findByEmail(String mail);
 
     void remove(User u);
-    /*
-@Service
-public class UserService {
-
-    @Autowired
-    private UserRepository userRepository;
-
-
-    public User findOne(Long id){
-        return userRepository.findById(id).orElseGet(null);
-    }
-
-    public List<User> findAll(){
-        return userRepository .findAll();
-    }
-
-    public User findByEmail(String email){
-
-        User user = userRepository.findByEmail(email);
-
-        return user;
-
-    }
-
-    public User findByUserName(String userName){
-        for(User u : userRepository.findAll()){
-            if(u.getUserName().equals(userName)){
-                return u;
-            }
-        }
-        return null;
-    }
-
-    public User findByUICDN(String uicdn){
-        for(User u : userRepository.findAll()){
-            if(u.getUcidn().equals(uicdn)){
-                return u;
-            }
-        }
-        return null;
-    }
-
-
-    public User save(User u) {
-        return userRepository.save(u);
-    }
-
-    public void remove(Long id){
-        userRepository.deleteById(id);
-    }
-*/
 
 }
 
