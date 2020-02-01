ee528361eaf86eaf36a092d5868a5bad0da2e319
miilica <milicaradovanovic64@gmail.com>
2020-02-02 00:23:21 +0100

    Update RequestForConsultDTO.java

--- ClinicalCenter/ClinicalCenterApp/src/main/java/com/softwareComedians/ClinicalCenterApp/dto/RequestForConsultDTO.java
+++ ClinicalCenter/ClinicalCenterApp/src/main/java/com/softwareComedians/ClinicalCenterApp/dto/RequestForConsultDTO.java
@@ -7,25 +7,41 @@ import java.sql.Date;
 public class RequestForConsultDTO {
     private Date dateAndTime;
     private boolean isAccepted;
-    private  String type;
+    private  ConsultTypeDTO type;
     private Long id;
+    //private String applicant;
+    private UserDTO patient;
+
+    public UserDTO getPatient() {
+        return patient;
+    }
+
+    public void setPatient(UserDTO patient) {
+        this.patient = patient;
+    }
 
     public RequestForConsultDTO() {
     }
 
-    public RequestForConsultDTO(Date dateAndTime, boolean isAccepted, String type, Long id) {
+    public RequestForConsultDTO(Date dateAndTime, boolean isAccepted, ConsultTypeDTO type, Long id, UserDTO p) {
         this.dateAndTime = dateAndTime;
         this.isAccepted = isAccepted;
         this.type = type;
         this.id=id;
+       // this.applicant = u;
+        this.patient = p;
     }
 
     public RequestForConsultDTO(RequestForConsult rfc) {
        dateAndTime = rfc.getDateAndTime();
        isAccepted = rfc.isAccepted();
-       type = rfc.getType();
+       type = new ConsultTypeDTO(rfc.getType());
        id=rfc.getId();
+       patient = new UserDTO(rfc.getPatient());
+       //applicant = new UserDTO(rfc.getApplicant());
+
     }
+
 
     public Date getDateAndTime() {
         return dateAndTime;
@@ -51,11 +67,12 @@ public class RequestForConsultDTO {
         isAccepted = accepted;
     }
 
-    public String getType() {
+    public ConsultTypeDTO getType() {
         return type;
     }
 
-    public void setType(String type) {
+    public void setType(ConsultTypeDTO type) {
         this.type = type;
     }
+
 }
