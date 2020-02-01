e946cfe28c57611e668f5dbe34b49e47d18a42d2
miilica <milicaradovanovic64@gmail.com>
2020-02-02 00:23:49 +0100

    Update ReqestForConsultController.java

--- ClinicalCenter/ClinicalCenterApp/src/main/java/com/softwareComedians/ClinicalCenterApp/controller/ReqestForConsultController.java
+++ ClinicalCenter/ClinicalCenterApp/src/main/java/com/softwareComedians/ClinicalCenterApp/controller/ReqestForConsultController.java
@@ -1,9 +1,9 @@
 package com.softwareComedians.ClinicalCenterApp.controller;
 
 import com.softwareComedians.ClinicalCenterApp.dto.RequestForConsultDTO;
-import com.softwareComedians.ClinicalCenterApp.dto.UserDTO;
+import com.softwareComedians.ClinicalCenterApp.mail.SmtpMailSender;
 import com.softwareComedians.ClinicalCenterApp.model.RequestForConsult;
-import com.softwareComedians.ClinicalCenterApp.model.User;
+import com.softwareComedians.ClinicalCenterApp.service.ConsultTypeService;
 import com.softwareComedians.ClinicalCenterApp.service.RequestForConsultService;
 import com.softwareComedians.ClinicalCenterApp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,13 +19,38 @@ public class ReqestForConsultController {
     @Autowired
     private RequestForConsultService requestForConsultService;
 
+    @Autowired
+    private ConsultTypeService consultTypeService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private SmtpMailSender smtpMailSender;
+
     @PostMapping()
     public ResponseEntity<RequestForConsultDTO> createRequest(@RequestBody RequestForConsultDTO requestForConsultDTO) {
 
         RequestForConsult rq = new RequestForConsult();
         rq.setId(requestForConsultDTO.getId());
-        rq.setDateAndTime(requestForConsultDTO.getDateAndTime());
-        rq.setType(requestForConsultDTO.getType());
+       // rq.setDateAndTime(requestForConsultDTO.getDateAndTime());
+        rq.setType(consultTypeService.findOne(requestForConsultDTO.getType().getId()));
+       // rq.setPatient(userService.findById(requestForConsultDTO.getPatient().getId()));
+
+        rq = requestForConsultService.save(rq);
+        return new ResponseEntity<>(new RequestForConsultDTO(rq), HttpStatus.CREATED);
+    }
+
+    @PostMapping(value = "doctor")
+    public ResponseEntity<RequestForConsultDTO> createRequestDoctor(@RequestBody RequestForConsultDTO requestForConsultDTO) {
+
+        RequestForConsult rq = new RequestForConsult();
+        rq.setId(requestForConsultDTO.getId());
+        // rq.setDateAndTime(requestForConsultDTO.getDateAndTime());
+        rq.setType(consultTypeService.findOne(requestForConsultDTO.getType().getId()));
+        rq.setPatient(userService.findById(requestForConsultDTO.getPatient().getId()));
+        //send mail, getPatient.getClicic.getAdmin.getMail
+        //smtpMailSender.send(requestForConsultDTO.get,"Registration", description);
 
         rq = requestForConsultService.save(rq);
         return new ResponseEntity<>(new RequestForConsultDTO(rq), HttpStatus.CREATED);
