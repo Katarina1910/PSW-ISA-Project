355c263b37ae98b08561d57f67d0b4d21ff8d8b9
miilica <milicaradovanovic64@gmail.com>
2020-02-02 00:19:44 +0100

    Create doctorRequestForConsult.service.ts

---
+++ APM/src/app/doctorRequestForConsult/doctorRequestForConsult.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DoctorRequestForConsult } from './doctorRequestForConsult';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DoctorRequestForConsultService{
+    _url = 'http://localhost:8080/api/rqForConsult';
+
+    constructor(private _http: HttpClient) { }
+
+    addRequestForConsult(requestForConsult: DoctorRequestForConsult) {
+       return  this._http.post<any>(this._url, requestForConsult);
+    }
+}
