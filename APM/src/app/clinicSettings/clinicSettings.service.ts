2e52fc5d212798e072cb8c87584e44933a5d65f4
miilica <milicaradovanovic64@gmail.com>
2020-02-02 17:12:32 +0100

    Create clinicSettings.service.ts

---
+++ APM/src/app/clinicSettings/clinicSettings.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../registration/user';
+import { Observable } from 'rxjs';
+import { Clinic } from '../addNewClinic/clinic';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class ClinicSettingsService{
+    user: User = new User("","","","","","","","","","","","");
+    id: string = this.user.id;
+
+    _url = 'http://localhost:8080/api/clinics/edit';
+    _url1 = 'http://localhost:8080/api/clinics/get';
+
+    constructor(private _http: HttpClient) { }
+
+
+
+    editClinic(clinic: Clinic) : Observable<Clinic> {
+        return  this._http.put<Clinic>(this._url, clinic);
+    }
+
+    getClinic(): Observable<any>{
+        return this._http.get<Clinic>(`${this._url1}/${this.id}`);
+    }
+
+    
+}
