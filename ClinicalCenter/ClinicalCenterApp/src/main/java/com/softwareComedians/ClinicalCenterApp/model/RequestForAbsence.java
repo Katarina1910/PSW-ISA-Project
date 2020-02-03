6a59857e267395c7d1e41e35f68b7e682e2181be
miilica <milicaradovanovic64@gmail.com>
2020-02-03 01:21:54 +0100

    made getters and setters

--- ClinicalCenter/ClinicalCenterApp/src/main/java/com/softwareComedians/ClinicalCenterApp/model/RequestForAbsence.java
+++ ClinicalCenter/ClinicalCenterApp/src/main/java/com/softwareComedians/ClinicalCenterApp/model/RequestForAbsence.java
@@ -2,8 +2,10 @@ package com.softwareComedians.ClinicalCenterApp.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Getter
@@ -22,6 +24,14 @@ public class RequestForAbsence {
 	@Column
 	private String resaonOfRejection;
 
+	@Column
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date from;
+
+	@Column
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date to;
+
 	@ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
 	private ClinicAdministrator clinicAdministrator;
 	
@@ -29,6 +39,59 @@ public class RequestForAbsence {
 		super();
 	}
 
+	public Long getId() {
+		return id;
+	}
 
-	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Personnel getApplicant() {
+		return applicant;
+	}
+
+	public void setApplicant(Personnel applicant) {
+		this.applicant = applicant;
+	}
+
+	public boolean isAccepted() {
+		return isAccepted;
+	}
+
+	public void setAccepted(boolean accepted) {
+		isAccepted = accepted;
+	}
+
+	public String getResaonOfRejection() {
+		return resaonOfRejection;
+	}
+
+	public void setResaonOfRejection(String resaonOfRejection) {
+		this.resaonOfRejection = resaonOfRejection;
+	}
+
+	public Date getFrom() {
+		return from;
+	}
+
+	public void setFrom(Date from) {
+		this.from = from;
+	}
+
+	public Date getTo() {
+		return to;
+	}
+
+	public void setTo(Date to) {
+		this.to = to;
+	}
+
+	public ClinicAdministrator getClinicAdministrator() {
+		return clinicAdministrator;
+	}
+
+	public void setClinicAdministrator(ClinicAdministrator clinicAdministrator) {
+		this.clinicAdministrator = clinicAdministrator;
+	}
 }
