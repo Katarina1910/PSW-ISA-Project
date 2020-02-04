39e5b98456edffc89ef09948e8a51597b1b6cd86
miilica <milicaradovanovic64@gmail.com>
2020-02-04 14:04:22 +0100

    Create RoomTermRepository.java

---
+++ ClinicalCenter/ClinicalCenterApp/src/main/java/com/softwareComedians/ClinicalCenterApp/repository/RoomTermRepository.java
@@ -0,0 +1,8 @@
+package com.softwareComedians.ClinicalCenterApp.repository;
+
+import com.softwareComedians.ClinicalCenterApp.model.RoomTerms;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomTermRepository extends JpaRepository<RoomTerms, Long> {
+
+}
