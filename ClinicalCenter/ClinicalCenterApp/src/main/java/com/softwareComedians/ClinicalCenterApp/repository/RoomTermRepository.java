df0a6aeb28f968f2995723d1fec852cb3dcbb72d
miilica <milicaradovanovic64@gmail.com>
2020-02-04 16:37:40 +0100

    add findByDate

--- ClinicalCenter/ClinicalCenterApp/src/main/java/com/softwareComedians/ClinicalCenterApp/repository/RoomTermRepository.java
+++ ClinicalCenter/ClinicalCenterApp/src/main/java/com/softwareComedians/ClinicalCenterApp/repository/RoomTermRepository.java
@@ -3,6 +3,9 @@ package com.softwareComedians.ClinicalCenterApp.repository;
 import com.softwareComedians.ClinicalCenterApp.model.RoomTerms;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RoomTermRepository extends JpaRepository<RoomTerms, Long> {
+    List<RoomTerms> findByDate(String date);
 
 }
