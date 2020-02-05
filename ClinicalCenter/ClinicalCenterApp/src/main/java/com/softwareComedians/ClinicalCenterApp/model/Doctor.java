766da4d5ee2c6dd5cee0eb17f79369996dcd7098
miilica <milicaradovanovic64@gmail.com>
2020-02-05 14:52:38 +0100

    add field doctorTerms

--- ClinicalCenter/ClinicalCenterApp/src/main/java/com/softwareComedians/ClinicalCenterApp/model/Doctor.java
+++ ClinicalCenter/ClinicalCenterApp/src/main/java/com/softwareComedians/ClinicalCenterApp/model/Doctor.java
@@ -8,7 +8,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
-import java.util.Date;
 import java.util.Set;
 
 @Entity
@@ -23,8 +22,6 @@ public class Doctor extends Personnel {
     @Column
     private Double grade;
 
-  //  @OneToMany(mappedBy = "applicant", fetch = FetchType.LAZY , cascade = CascadeType.ALL)
-   // private Set<RequstForOperation> requstForOperations;
     @Column
     private Double typeId;
 
@@ -32,10 +29,11 @@ public class Doctor extends Personnel {
     private Timestamp scheduledFrom;    //od kog datuma je zauzet
 
     @Column
-    private Timestamp scheduledTo;      //do kog datuma je zauzet
+    private Timestamp scheduledTo;
 
-    //@OneToMany(mappedBy = "applicant", fetch = FetchType.LAZY , cascade = CascadeType.ALL)
-   // private Set<RequstForOperation> requstForOperations;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "doctor")
+    private Set<DoctorTerms> doctorTerms;
+
 
     @OneToMany(mappedBy = "doctor", fetch = FetchType.LAZY , cascade = CascadeType.ALL)
     private Set<ConsultTerm> consultTerms;
@@ -46,6 +44,32 @@ public class Doctor extends Personnel {
    // private Set<Operation> operations;
 
     public Doctor() {
+    }
+
+    public Doctor(DoctorDTO d){
+        this.setId(d.getId());
+        this.setName(d.getName());
+        this.setSurname((d.getSurname()));
+        this.setUcidn(d.getUcidn());
+        this.setEmail(d.getEmail());
+        this.setPassword(passwordEncoder.encode(d.getPassword()));
+        this.setUsername(d.getUsername());
+        this.setPhone(d.getPhone());
+        this.setAddress(d.getAddress());
+        this.setCity(d.getCity());
+        this.setCountry(d.getCountry());
+        this.setActivated(d.isActivated());
+        this.grade = 0.0;
+        this.typeId = null;
+        this.setRole("ROLE_DOCTOR");
+    }
+
+    public PasswordEncoder getPasswordEncoder() {
+        return passwordEncoder;
+    }
+
+    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Double getGrade() {
@@ -64,24 +88,35 @@ public class Doctor extends Personnel {
         this.typeId = typeId;
     }
 
-    public Doctor(DoctorDTO d){
-        this.setId(d.getId());
-        this.setName(d.getName());
-        this.setSurname((d.getSurname()));
-        this.setUcidn(d.getUcidn());
-        this.setEmail(d.getEmail());
-        this.setPassword(passwordEncoder.encode(d.getPassword()));
-        this.setUsername(d.getUsername());
-        this.setPhone(d.getPhone());
-        this.setAddress(d.getAddress());
-        this.setCity(d.getCity());
-        this.setCountry(d.getCountry());
-        this.setActivated(d.isActivated());
-        this.grade = 0.0;
-        this.typeId = null;
-        this.setRole("DOCTOR");
-
+    public Timestamp getScheduledFrom() {
+        return scheduledFrom;
     }
 
+    public void setScheduledFrom(Timestamp scheduledFrom) {
+        this.scheduledFrom = scheduledFrom;
+    }
 
+    public Timestamp getScheduledTo() {
+        return scheduledTo;
+    }
+
+    public void setScheduledTo(Timestamp scheduledTo) {
+        this.scheduledTo = scheduledTo;
+    }
+
+    public Set<DoctorTerms> getDoctorTerms() {
+        return doctorTerms;
+    }
+
+    public void setDoctorTerms(Set<DoctorTerms> doctorTerms) {
+        this.doctorTerms = doctorTerms;
+    }
+
+    public Set<ConsultTerm> getConsultTerms() {
+        return consultTerms;
+    }
+
+    public void setConsultTerms(Set<ConsultTerm> consultTerms) {
+        this.consultTerms = consultTerms;
+    }
 }
