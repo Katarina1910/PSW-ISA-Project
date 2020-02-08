651332f996adbd835e9a0fc3967023d82a766601
miilica <milicaradovanovic64@gmail.com>
2020-02-08 12:50:58 +0100

    Create ClinicsServiceTest.java

---
+++ ClinicalCenter/ClinicalCenterApp/src/test/java/com/softwareComedians/ClinicalCenterApp/service/ClinicsServiceTest.java
@@ -0,0 +1,36 @@
+package com.softwareComedians.ClinicalCenterApp.service;
+
+import com.softwareComedians.ClinicalCenterApp.model.Clinic;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ClinicsServiceTest {
+    @Autowired
+    private  ClinicsService clinicsService;
+
+    @Test
+    public void findAll() {
+        List<Clinic> clinics = (List<Clinic>) clinicsService.findAll();
+        assertEquals(clinics.size(), 3);
+    }
+    @Test
+    public void findById() {
+        Clinic clinic = clinicsService.findById(1L);
+
+        assertEquals(clinic.getId() , 1L);
+        assertEquals(clinic.getName(), "klinika1");
+
+
+    }
+}
+
+
