803caca7ad939c055c98a764f375036e210fafa5
Katarina1910 <katarinaprodanovic97@gmail.com>
2020-02-08 16:20:50 +0100

    Get vacation to calendar

--- APM/src/app/doctorWorkingCalendar/doctorWorkCal.service.ts
+++ APM/src/app/doctorWorkingCalendar/doctorWorkCal.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ConsultTerm } from '../consultTerm/consultTerm';
+import { RequestForAbsence } from '../requestForAbsence/requestForAbsence';
 
 @Injectable({
     providedIn: 'root'
@@ -11,6 +12,7 @@ export class DoctorWorkCalService{
     public consults: ConsultTerm[];
     _url = 'http://localhost:8080/api/ConsultTerm/getConsults';
     _url1 = 'http://localhost:8080/api/ConsultTerm/getConsult';
+    _url2 = 'http://localhost:8080/api/RqForAbsence/getAll';
  
     constructor(private _http: HttpClient) { }
 
@@ -20,5 +22,9 @@ export class DoctorWorkCalService{
 
     getConsult(id: any):Observable<any> {
         return this._http.get<ConsultTerm>(`${this._url1}/${id}`);
+    }
+
+    getVacations(id: any):Observable<any> {
+        return this._http.get<RequestForAbsence[]>(`${this._url2}/${id}`);
     }
 }
