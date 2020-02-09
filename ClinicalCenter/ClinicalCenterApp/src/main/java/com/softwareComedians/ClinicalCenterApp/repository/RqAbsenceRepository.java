ed5888fd9c760e95ea6dd40c703754e0ce8823f7
Katarina1910 <katarinaprodanovic97@gmail.com>
2020-02-09 12:50:49 +0100

    Fixed request for absence

--- ClinicalCenter/ClinicalCenterApp/src/main/java/com/softwareComedians/ClinicalCenterApp/repository/RqAbsenceRepository.java
+++ ClinicalCenter/ClinicalCenterApp/src/main/java/com/softwareComedians/ClinicalCenterApp/repository/RqAbsenceRepository.java
@@ -2,6 +2,14 @@ package com.softwareComedians.ClinicalCenterApp.repository;
 
 import com.softwareComedians.ClinicalCenterApp.model.RequestForAbsence;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface RqAbsenceRepository extends JpaRepository<RequestForAbsence, Long> {
+
+    @Query("select r from RequestForAbsence r where r.applicant.id =?1")
+    List<RequestForAbsence> findAllByUserId(Long id);
 }
